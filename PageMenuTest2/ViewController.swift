126d6f380346f62e920f2061d371fb6816b15b21
Emyl Khairuddin <emylkhairuddin@emyls-macbook-pro.local>
2018-07-12 10:55:27 +0800

    Initial Commit

---
+++ PageMenuTest2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageMenuTest2
+//
+//  Created by Emyl Khairuddin on 12/07/2018.
+//  Copyright © 2018 Emyl Khairuddin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
